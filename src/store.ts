fcab60a6878e9192e64baa4dd54d418452edeab8
Iana <iankuzn21@gmail.com>
2025-03-11 21:29:23 +0500

    исправление dataStore, возвращение динамики

--- src/store.ts
+++ src/store.ts
@@ -3,8 +3,5 @@ import { createStore, createEvent } from 'effector';
 export const setData = createEvent<any[]>();
 
 export const $dataStore = createStore<any[]>([]).on(setData, (_, data) => {
-  if (_.length === 0) {
-    return data;
-  }
-  return _;
+  return data;
 });
